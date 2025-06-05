bd6107f29336066d5485758358058ea003630d4e
Allaoui Sami <sami.allaoui@icloud.com>
2025-06-05 15:08:18 +0200

    te

--- docs/swagger.js
+++ docs/swagger.js
@@ -1,6 +1,7 @@
 // doc/swagger.js
 const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
+const path = require("path");
 
 const options = {
   definition: {
